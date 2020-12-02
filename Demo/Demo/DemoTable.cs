774d7265315fe55d9522063c848f2a2886c18891
覃欢 <1759042367@qq.com>
2020-12-03 07:55:28 +0800

    demo

--- Demo/Demo/DemoTable.cs
+++ Demo/Demo/DemoTable.cs
@@ -1,7 +1,19 @@
-﻿namespace Demo
+﻿using System;
+using DbStorage.Interface;
+using FreeSql.DataAnnotations;
+
+namespace Demo
 {
-    public class DemoTable
+    public class DemoTable : IDbEntity<long>,IPartitionEntity
     {
-        
+        [Column(IsIdentity = true, IsPrimary = true)]
+        public long Id { get; set; }
+        public object PartitionId { get; set; }
+
+        public string Remark { get; set; }
+
+        public long FkId { get; set; }
+
+        public DateTime CreateTime { get; set; }
     }
 }
--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using DbStorage;
 using DbStorage.Impl;
 using FreeSqlDbStorage;
@@ -36,6 +37,54 @@ namespace Demo
 
             freeSql.CodeFirst.SyncStructure<DemoTable>();
 
+            CreateInitDemoData(freeSql);
+
+
+            //编写一个查询语句
+
+            
+            Test(freeSql);
+
+        }
+
+
+        private static void Test(IFreeSql freeSql)
+        {
+
+            var data = freeSql.Select<DemoTable>()
+                .Where(x => x.CreateTime.Date == DateTime.Now.Date).ToList();
+
+
+
+
+        }
+
+
+        /// <summary>
+        /// 创建测试数据
+        /// </summary>
+        /// <param name="freeSql"></param>
+        private static void CreateInitDemoData(IFreeSql freeSql)
+        {
+            var date = DateTime.Now.Date;
+
+            var data = new List<DemoTable>();
+
+            for (int i = 0; i < 24; i++)
+            {
+                for (int j = 0; j < 10000; j++)
+                {
+                    data.Add(new DemoTable()
+                    {
+                        CreateTime = date.AddHours(i),
+                        FkId = j,
+                        PartitionId = date.AddHours(i),
+                        Remark = $"Demo程序演示表数据{i}{j}"
+                    });
+                }
+            }
+
+            freeSql.Insert(data).ExecuteAffrows();
         }
     }
 }
